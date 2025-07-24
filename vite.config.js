3c3461d357bf2624149eeed8248771c6c1c9696a
Bassirou Sagna <sagnab034@gmail.com>
2025-07-24 13:47:06 +0800

    Fix vite base path for Vercel

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: './', // ✅ THIS IS REQUIRED FOR VERCEL STATIC HOSTING
   plugins: [react()],
 })
